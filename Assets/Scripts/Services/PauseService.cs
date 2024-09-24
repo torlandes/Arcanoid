c41a660b4995278cefcd9b35a15734673b2cbf0c
torlandes <torlandesx@gmial.com>
2024-09-24 18:38:09 +0300

    Added pause service script

---
+++ Assets/Scripts/Services/PauseService.cs
@@ -0,0 +1,36 @@
+﻿using Arcanoid.Utility;
+using UnityEngine;
+
+namespace Arcanoid.Services
+{
+    public class PauseService : SingletonMonoBehaviour<PauseService>
+    {
+        #region Variables
+
+        private bool _isPaused;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                TogglePause();
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void TogglePause()
+        {
+            _isPaused = !_isPaused;
+            Time.timeScale = _isPaused ? 0 : 1;
+        }
+
+        #endregion
+    }
+}
