582827e5dfb8a985ac51571e3b272b524107a4c7
torlandes <torlandesx@gmial.com>
2024-10-08 00:01:00 +0300

    Added UI scripts

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,44 @@
+﻿using Arcanoid.Services;
+using TMPro;
+using UnityEngine;
+
+namespace Arcanoid.UI
+{
+    public class GameScreen : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private TMP_Text _scoreLabel;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Start()
+        {
+            GameService.Instance.OnScoreChanged += ScoreChangedCallback;
+            UpdateScore();
+        }
+
+        private void OnDestroy()
+        {
+            GameService.Instance.OnScoreChanged -= ScoreChangedCallback;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void ScoreChangedCallback(int score)
+        {
+            UpdateScore();
+        }
+
+        private void UpdateScore()
+        {
+            _scoreLabel.text = $"Score: {GameService.Instance.Score}";
+        }
+
+        #endregion
+    }
+}
