fe08b4202f508bc7650423a616bbfbf8fccb78e1
torlandes <torlandesx@gmial.com>
2024-11-11 22:37:12 +0300

    Added audio service

---
+++ Assets/Scripts/Services/AudioService.cs
@@ -0,0 +1,27 @@
+﻿using Arcanoid.Utility;
+using UnityEngine;
+
+namespace Arcanoid.Services
+{
+    public class AudioService : SingletonMonoBehaviour<AudioService>
+    {
+        #region Variables
+
+        [SerializeField] private AudioSource _sfxAudioSource;
+
+        #endregion
+
+        #region Public methods
+
+        public void PlaySfx(AudioClip clip)
+        {
+            if (clip == null)
+            {
+                return;
+            }
+            _sfxAudioSource.PlayOneShot(clip);
+        }
+
+        #endregion
+    }
+}
