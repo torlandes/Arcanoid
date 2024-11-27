11fc0e6fc80d58bd05e150de042df696f5e87624
torlandes <torlandesx@gmial.com>
2024-11-27 04:23:33 +0300

    Fixde script for tag

--- Assets/Scripts/Game/Tag.cs
+++ Assets/Scripts/Game/Tag.cs
@@ -5,6 +5,7 @@
         #region Variables
 
         public const string Ball = "Ball";
+        public const string Block = "Block";
         public const string Platform = "Platform";
 
         #endregion
